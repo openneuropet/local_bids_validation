f5657a0738d860b3db3b76f35b786594689376ad
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2023-08-02 11:36:00 -0400

    removed submodule

---
+++ ds004564-1.0.1.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds004564/CHANGES?versionId=qmm3QsruyXxy_uYpamDEiaecn1AA1ktz -o CHANGES
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds004564/README?versionId=QdNGLf50f0Obg28aTEPlUZL16kPpXLCM -o README
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds004564/dataset_description.json?versionId=LqUWYCmYTSGB0d0DDMJjAX7IWnXMleSo -o dataset_description.json
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds004564/participants.tsv?versionId=z8xsU.AOXaJjBOwmh1G_NWN7U9y7C2SL -o participants.tsv
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds004564/sub-17017/anat/sub-17017_T1w.nii.gz?versionId=yA0nqrWFmMBOc7_1VT5EykvSHShkKOt9 -o sub-17017/anat/sub-17017_T1w.nii.gz
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds004564/sub-19738/func/sub-19738_task-rest_bold.json?versionId=BlNfyFPJgB.TKFW2PY7KllT22DFJVVb4 -o sub-17017/func/sub-17017_task-rest_bold.json
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds004564/sub-17017/func/sub-17017_task-rest_bold.nii.gz?versionId=uo8Ynu8W6lfnQyQjfW4z3NkPVkA5tQ9Q -o sub-17017/func/sub-17017_task-rest_bold.nii.gz
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds004564/sub-19738/anat/sub-19738_T1w.nii.gz?versionId=CiqxCZfc6CWFzDYAyeOVyR9bhIOmbY_d -o sub-19738/anat/sub-19738_T1w.nii.gz
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds004564/sub-19738/func/sub-19738_task-rest_bold.json?versionId=BlNfyFPJgB.TKFW2PY7KllT22DFJVVb4 -o sub-19738/func/sub-19738_task-rest_bold.json
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds004564/sub-19738/func/sub-19738_task-rest_bold.nii.gz?versionId=_aiOwE124ilAuQvoYRV2LwcMpM3yVxBu -o sub-19738/func/sub-19738_task-rest_bold.nii.gz
